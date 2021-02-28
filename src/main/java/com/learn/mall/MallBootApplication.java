bff3afed3d0397bb9ceb019e8f1c45f79322aea2
WHU-gentle <1694272297@qq.com>
2021-02-28 16:26:56 +0800

    Inite

---
+++ src/main/java/com/learn/mall/MallBootApplication.java
@@ -0,0 +1,13 @@
+package com.learn.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/mall/MallBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
